240f022c7b45acaa6d5c2298a064553deec2fed8
natohot1 <nato_hot1@yahoo.es>
2021-11-17 13:57:42 +0100

    Registro completo, primar parte foto

--- app/src/main/java/com/example/segundo/MainActivity.kt
+++ app/src/main/java/com/example/segundo/MainActivity.kt
@@ -1,16 +1,76 @@
 package com.example.segundo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
-
+data class Usuarios (val user:String, val email:String, val id:String)
 class MainActivity : AppCompatActivity() {
+
+    private var autentication : FirebaseAuth?=null
+    private var database = FirebaseDatabase.getInstance()
+    private var conexion = database.reference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mio = correoid.text.toString()
+        autentication = FirebaseAuth.getInstance()
 
 
+
+        //  FUNCION DE REGISTRO
+        buttonRegistro.setOnClickListener {
+            val email = correoid.text.toString()
+            val pasword = contrasenaId.text.toString()
+            login(email,pasword)
+        }
+
+        // LLAMA A INCIO DE SESION
+        buttonInicio.setOnClickListener {
+            val email = correoid.text.toString()
+            val pasword = contrasenaId.text.toString()
+            inicio(email,pasword)
+        }
+        
     }
+
+    private fun inicio(email: String, pasword: String) {
+        autentication!!.signInWithEmailAndPassword(email,pasword).addOnCompleteListener {
+            if (it.isSuccessful){
+                val idUser = FirebaseAuth.getInstance().currentUser!!.uid
+                Toast.makeText(applicationContext, "Has Iniciado sesion", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,SegundaActivity::class.java)
+                intent.putExtra("email",email)
+                startActivity(intent)
+            }else{
+                Toast.makeText(applicationContext, "Ha fallado, no se pudo iniciar", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun login(email: String, pasword: String){
+        autentication!!.createUserWithEmailAndPassword(email,pasword).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val Uid = FirebaseAuth.getInstance().currentUser!!.uid
+                Toast.makeText(applicationContext, "Registrado", Toast.LENGTH_SHORT).show()
+              //  conexion.child("users").child(Uid).setValue(Usuarios(user = String(),email,Uid))
+                val intent = Intent(this,SegundaActivity::class.java)
+                intent.putExtra("email",email)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(applicationContext, "Ha fallado el Registro", Toast.LENGTH_SHORT).show()
+            }
+            
+        }
+    }
+    
+    
+    
+    
 }
--- app/src/main/java/com/example/segundo/SegundaActivity.kt
+++ app/src/main/java/com/example/segundo/SegundaActivity.kt
@@ -1,11 +1,36 @@
 package com.example.segundo
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_segunda.*
 
+private const val RECUEST_CODE = 42
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        btnFotos.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent.resolveActivity(this.packageManager) != null){
+                startActivityForResult(takePictureIntent, RECUEST_CODE)
+            } else{
+                Toast.makeText(this,"No se puede abrir camara", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RECUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val takenImage = data?.extras?.get("data") as Bitmap
+            img1.setImageBitmap(takenImage)
+        }else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
